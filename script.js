d770e2f8ba63e7730a98bb8e7ce6102739bf81a9
Juwon <lordjuey@gmail.com>
2020-07-19 16:10:22 +1000

    final version with links to github and linkedIN

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('#github').addEventListener('click', function(){
+    location.href='https://github.com/lordjuey';
+})
+
+document.querySelector('#linkedIn').addEventListener('click', function(){
+    location.href='https://www.linkedin.com/in/juwon-hong-a275a41b2/';
+})
